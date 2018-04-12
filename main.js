5c8a35ba9fdf4e3fe4c48cc6d1851ce54d2f473f
Greg Bergé <berge.greg@gmail.com>
2018-04-12 14:27:45 +0200

    [ex-4] arrays

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const article = {
-  title: 'My first article',
-  content: 'This is my awesome article',
+const articles = [
+  {
+    title: 'My first article',
+    content: 'Small content',
+  },
+  {
+    title: 'My second article',
+    content: 'Very very very long content',
+  },
+  {
+    title: 'My third article',
+    content: 'Not very long content',
+  },
+]
+
+function resumeArticle({ title, content }) {
+  return `${title} (${content.length} words)`
 }
 
-const { title, content } = article
+function showArticlesWithMinSign(articles, minSign) {
+  return articles
+    .filter(article => article.content.length > minSign)
+    .map(resumeArticle)
+    .join(', ')
+}
 
-console.log(`${title} (${content.length} words)`)
+console.log(showArticlesWithMinSign(articles, 20))
+// My second article (27 words), My third article (21 words)
