c02026684032f8206979e28caa09da0dd957113c
Greg Bergé <berge.greg@gmail.com>
2018-04-12 14:36:43 +0200

    [ex-5] classes

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
+class Article {
+  constructor({ title, content }) {
+    this.title = title
+    this.content = content
+  }
+
+  countSigns() {
+    return this.content.length
+  }
+
+  resume() {
+    return `${this.title} (${this.countSigns()} words)`
+  }
+}
+
 const articles = [
-  {
+  new Article({
     title: 'My first article',
     content: 'Small content',
-  },
-  {
+  }),
+  new Article({
     title: 'My second article',
     content: 'Very very very long content',
-  },
-  {
+  }),
+  new Article({
     title: 'My third article',
     content: 'Not very long content',
-  },
+  }),
 ]
-
-function resumeArticle({ title, content }) {
-  return `${title} (${content.length} words)`
-}
 
 function showArticlesWithMinSign(articles, minSign) {
   return articles
-    .filter(article => article.content.length > minSign)
-    .map(resumeArticle)
+    .filter(article => article.countSigns() > minSign)
+    .map(article => article.resume())
     .join(', ')
 }
 
