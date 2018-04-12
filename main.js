6488d217bb69c81dc3b045590b55388734507d9b
Greg Bergé <berge.greg@gmail.com>
2018-04-12 14:06:46 +0200

    [ex-2] objects

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('Hello world!')
+const article = {
+  title: 'My first article',
+  content: 'This is my awesome article',
+}
+
+console.log(article.title + ' (' + article.content.length + ' words)')
